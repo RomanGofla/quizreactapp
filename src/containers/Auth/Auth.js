fb9f07235dddd97c3fee9ce5706f5c69c457e1e0
Roman Gofla <gofla95@list.ru>
2021-03-14 10:27:16 +0300

    Navigation done

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,11 @@
+import { render } from '@testing-library/react'
+import React, { Component } from 'react'
+import classes from './Auth.module.css'
+
+export default class Auth extends Component {
+    render() {
+        return (
+            <h1>Auth</h1>
+        )
+    }
+}
